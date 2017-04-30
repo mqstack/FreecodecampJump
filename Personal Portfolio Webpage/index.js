f4850e8836e1501d9b7fbf777699c8f320e46bc4
mqstack <mqstack@outlook.com>
2017-04-30 23:31:17 +0800

    Add Portfolio

---
+++ Personal Portfolio Webpage/index.js
@@ -0,0 +1,51 @@
+$(window).on('resize', function(){
+	'use strict';
+	var element = document.querySelector('#banner'),
+	elHeight = 0,
+	elTop = 0,
+	dHeight = 0,
+	wHeight = 0,
+	wScrollCurrent = 0,
+	wScrollBefore = 0,
+	wScrollDiff = 0;
+	window.addEventListener('scroll', function(){
+		$('.navbar-collapse').collapse('hide');
+		$('.navbar-toggle').addClass('collapsed').blur();
+		if($(window).width() <= 768){
+
+		}
+		else element.style.top = '0px';
+	})
+
+}).resize();
+
+$(function() {
+	$("body")
+	.on("input propertychange", ".form-item", function(e){
+		$(this).toggleClass("form-item-filled", !! $(e.target).val());
+	})
+	.on("focus", ".form-item", function(){
+		$(this).addClass("form-item-focused");
+	})
+	.on("blur", ".form-item", function(){
+		$(THIS).removeClass("form-item-focused");
+	});
+});
+
+$('body').scrollspy({target: '.navbar-fixed-top'})
+
+$('.navbar-collapse ul li a').click(function(){
+	$(".navbar-collapse").collapse('hide');
+
+});
+
+$(function() {
+	$('body').on('click', 'a.scollable', function(event){
+		var $anchor = $(this);
+		$('html, body').stop().animate({
+			scrollTop: $($anchor.attr('href'))
+			.offset().top
+		}, 1500, 'easeInOutExpo');
+		event.preventDefault();
+	});
+});
